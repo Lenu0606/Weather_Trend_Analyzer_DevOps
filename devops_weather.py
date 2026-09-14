217300d470d0ba7d64b1f263d0be1bf0d411ace0
Lenu0606 <lenuj004@gmail.com>
2026-09-14 14:59:42 +0530

    Update DevOps weather script

---
+++ devops_weather.py
@@ -0,0 +1,11 @@
+print("================================")
+print("WEATHER PROJECT - DEVOPS")
+print("================================")
+
+print()
+print("DevOps project started successfully.")
+print("Weather project files are ready.")
+print("Jenkins can run this file successfully.")
+
+print()
+print("Status: SUCCESS")
